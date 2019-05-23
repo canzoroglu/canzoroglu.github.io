9bf7e2bf5fa9751aafb92ad88679e6869123b744
canzoroglu <canzoroglu@gmail.com>
2019-05-23 11:05:08 +0300

    Change table alignment, add js file

---
+++ main.js
@@ -0,0 +1,6 @@
+let navItemLink = document.querySelectorAll("li.nav-item a");
+let collapsedMenu = document.querySelector("nav div.collapse");
+
+navItemLink.forEach(item => {
+  item.addEventListener("click", () => collapsedMenu.classList.toggle("show"));
+});
